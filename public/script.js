cf901a6e188e6a73c32d4fbbbba03233df28e62e
indra <indralolx@gmail.com>
2024-07-21 22:05:58 +0800

    add promo section

--- public/script.js
+++ public/script.js
@@ -1,7 +1,6 @@
 window.addEventListener("scroll", function () {
   const navbar = document.getElementById("navbar-menu");
-  if (window.scrollY > 600) {
-    console.log("terscroll");
+  if (window.scrollY > 630) {
     navbar.classList.add("scrolled");
   } else {
     navbar.classList.remove("scrolled");
